b481a879fb4cfffa1585af9c11e46b89edbccbc6
Christopher Zielinski <chris@plugin.network>
2023-10-25 01:36:56 -0400

    Minor tweaks.

--- ActionAir/Components/NoctuaDataGrid.razor.cs
+++ ActionAir/Components/NoctuaDataGrid.razor.cs
@@ -1,35 +1,32 @@
 ﻿using ActionAir.Contracts;
 using Microsoft.AspNetCore.Components;
 
-namespace ActionAir.Components
+namespace ActionAir.Components;
+
+public partial class NoctuaDataGrid<TItem> : ComponentBase
 {
-    public partial class NoctuaDataGrid<TItem> : ComponentBase
+    // For parameters, dependency injection, and deferred initialization...
+    // ...we use the null-forgiving operator to make the compiler less chatty and happy.
+
+    [Parameter]
+    public string? Style { get; set; }
+
+    [Parameter]
+    public IEnumerable<TItem> Data { get; set; } = null!;
+
+    [Inject]
+    public IReflection Reflection { get; set; } = null!;
+
+    [Inject]
+    protected IHttpContextAccessor ContextAccessor { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
     {
-        // For parameters, dependency injection, and deferred initialization...
-        // ...we use the null-forgiving operator to make the compiler less chatty and happy.
-
-        [Parameter]
-        public string? Style { get; set; }
-
-        [Parameter]
-        public IEnumerable<TItem> Data { get; set; } = null!;
-
-        [Inject]
-        public IReflection Reflection { get; set; } = null!;
-
-
-        [Inject]
-        protected IHttpContextAccessor ContextAccessor { get; set; } = null!;
-
-
-        protected override async Task OnInitializedAsync()
-        {
-            await base.OnInitializedAsync();
-            FieldViewDescriptors = Reflection.GetFieldViewDescriptors(typeof(TItem));
-        }
-
-        protected bool IsPrerendering => ContextAccessor.HttpContext?.Response?.HasStarted ?? false;
-
-        private IList<IFieldViewDescriptor> FieldViewDescriptors { get; set; } = null!;
+        await base.OnInitializedAsync();
+        FieldViewDescriptors = Reflection.GetFieldViewDescriptors(typeof(TItem));
     }
+
+    protected bool IsPrerendering => ContextAccessor.HttpContext?.Response?.HasStarted ?? false;
+
+    private IList<IFieldViewDescriptor> FieldViewDescriptors { get; set; } = null!;
 }
--- ActionAir/Models/FieldViewDescriptor.cs
+++ ActionAir/Models/FieldViewDescriptor.cs
@@ -3,88 +3,87 @@ using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using ActionAir.Contracts;
 
-namespace ActionAir.Models
+namespace ActionAir.Models;
+
+public class FieldViewDescriptor : IFieldViewDescriptor
 {
-    public class FieldViewDescriptor : IFieldViewDescriptor
+    [SetsRequiredMembers]
+    public FieldViewDescriptor(Type classType, string fieldName) : this(fieldName)
     {
-        [SetsRequiredMembers]
-        public FieldViewDescriptor(Type classType, string fieldName) : this(fieldName)
-        {
-            var customAttributes = classType
-                ?.GetProperty(fieldName, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance)
-                ?.GetCustomAttributes();
+        var customAttributes = classType
+            ?.GetProperty(fieldName, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance)
+            ?.GetCustomAttributes();
 
-            Initialize(customAttributes);
-        }
-
-        [SetsRequiredMembers]
-        public FieldViewDescriptor(IEnumerable<Attribute>? customAttributes, string fieldName) : this(fieldName)
-        {
-            Initialize(customAttributes);
-        }
-
-        [SetsRequiredMembers]
-        protected FieldViewDescriptor(string fieldName)
-        {
-            Property = fieldName;
-            Title = fieldName.CreateLabelFromPropertyName();
-        }
-
-        private void Initialize(IEnumerable<Attribute>? customAttributes)
-        {
-            if (customAttributes == null)
-            {
-                return;
-            }
-
-            foreach (var customAttribute in customAttributes)
-            {
-                ProcessAttribute(customAttribute);
-            }
-        }
-
-        private void ProcessAttribute(Attribute? attribute)
-        {
-            if (attribute == null)
-            {
-                return;
-            }
-
-            switch (attribute)
-            {
-                case DisplayAttribute displayAttribute:
-                    Title = displayAttribute.Name ?? Title;
-                    AutoGenerateField = displayAttribute.GetAutoGenerateField() ?? AutoGenerateField;
-                    Filterable = displayAttribute.GetAutoGenerateFilter() ?? Filterable;
-                    Order = displayAttribute.GetOrder();
-                    break;
-
-                case DisplayFormatAttribute displayFormatAttribute:
-                    FormatString = displayFormatAttribute.DataFormatString ?? string.Empty;
-                    break;
-            }
-        }
-
-        public required string Property { get; set; }
-
-        private string? title;
-        public string Title
-        {
-            get => title ?? Property;
-            set => title = value;
-        }
-
-        public bool AutoGenerateField { get; set; } = true;
-
-        public bool Filterable { get; set; } = true;
-
-        private string? formatString;
-        public string FormatString
-        {
-            get => formatString ?? string.Empty;
-            set => formatString = value;
-        }
-
-        public int? Order { get; set; }
+        Initialize(customAttributes);
     }
+
+    [SetsRequiredMembers]
+    public FieldViewDescriptor(IEnumerable<Attribute>? customAttributes, string fieldName) : this(fieldName)
+    {
+        Initialize(customAttributes);
+    }
+
+    [SetsRequiredMembers]
+    protected FieldViewDescriptor(string fieldName)
+    {
+        Property = fieldName;
+        Title = fieldName.CreateLabelFromPropertyName();
+    }
+
+    private void Initialize(IEnumerable<Attribute>? customAttributes)
+    {
+        if (customAttributes == null)
+        {
+            return;
+        }
+
+        foreach (var customAttribute in customAttributes)
+        {
+            ProcessAttribute(customAttribute);
+        }
+    }
+
+    private void ProcessAttribute(Attribute? attribute)
+    {
+        if (attribute == null)
+        {
+            return;
+        }
+
+        switch (attribute)
+        {
+            case DisplayAttribute displayAttribute:
+                Title = displayAttribute.Name ?? Title;
+                AutoGenerateField = displayAttribute.GetAutoGenerateField() ?? AutoGenerateField;
+                Filterable = displayAttribute.GetAutoGenerateFilter() ?? Filterable;
+                Order = displayAttribute.GetOrder();
+                break;
+
+            case DisplayFormatAttribute displayFormatAttribute:
+                FormatString = displayFormatAttribute.DataFormatString ?? string.Empty;
+                break;
+        }
+    }
+
+    public required string Property { get; set; }
+
+    private string? title;
+    public string Title
+    {
+        get => title ?? Property;
+        set => title = value;
+    }
+
+    public bool AutoGenerateField { get; set; } = true;
+
+    public bool Filterable { get; set; } = true;
+
+    private string? formatString;
+    public string FormatString
+    {
+        get => formatString ?? string.Empty;
+        set => formatString = value;
+    }
+
+    public int? Order { get; set; }
 }
